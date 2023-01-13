9af063b69d5b6d3ba071b0507aba7a44c187a042
ozannicholls <116882906+ozannicholls@users.noreply.github.com>
2023-01-13 04:05:09 +0000

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,36 @@
+let particles = [];
+let d = 1;
+
 function setup() {
-    var canvas = createCanvas(800, 800);
+    var canvas = createCanvas(window.innerWidth, window.innerHeight);
     canvas.parent("p5container");
 }
 
 function draw() {
-    background(220);
+    background(8, 0, 15, 25);
 
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    let p = new Particle();
+    particles.push(p)
+    p.show()
+
+    // fill(255, 255, 255);
+    // ellipse(400, 400, 200);
+}
+
+class Particle{
+    constructor(){
+        this.x = MouseX;
+        this.y = MouseY;
+        this.vx = random(3, -3);
+        this.vy = random(2, -2);
+    }
+    update(){
+        this.x += this.vx
+        this.y += this.vy
+    }
+    show(){
+        noStroke()
+        fill(255, 150);
+        ellipse(this.x, this.y, d);
+    }
 }
